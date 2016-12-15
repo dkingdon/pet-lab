ab295f13a2f07fa9895c3bcf05f15e4e9f48c20c
dkingdon <daniel.kingdon@gmail.com>
2016-12-15 11:54:38 -0800

    pet to owner association and migration implemented and working

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,18 +1,21 @@
 class Owner < ActiveRecord::Base
   # TODO: add validations
 
+  has_many :pets
+
   before_save :normalize_phone_number
-
-  # removes leading 1 and the characters (, ), -, .
-  def normalize_phone_number
-    # if str.length = 11
-    #   str.shift
-    # end
-  end
-
   validates :first_name, presence: true, length: { maximum: 255 }
   validates :last_name, presence: true, length: { maximum: 255 }
   validates :email, presence: true, :format => /@/, uniqueness: true, length: { maximum: 255 }
+
+  # removes leading 1 and the characters (, ), -, .
+  def normalize_phone_number
+    if phone.present?
+      phone.gsub!(/^1/, "")
+      phone.gsub!(/[()-.]/,"")
+    end
+  end
+
 
 
 end
